63a1a348a9d6a19b74002db2b24ea7b6a71f495d
Malavathu Kodanda Krishna Sai <mkkrishnasai@gmail.com>
2023-06-29 17:19:01 +0530

    updated env

--- src/websocket/adapters/websocket.adapter.ts
+++ src/websocket/adapters/websocket.adapter.ts
@@ -1,5 +1,7 @@
 import { IoAdapter } from '@nestjs/platform-socket.io';
 import { ServerOptions } from 'socket.io';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 export class WebSocketAdapter extends IoAdapter {
   createIOServer(port: number, options?: ServerOptions): any {
